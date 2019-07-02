32dad5d04ad88fe9d9899e2cd7776f371ee764fa
Rohitupe <rohittupe21@gmail.com>
2019-07-02 15:09:46 +0530

    addition works

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,4 +3,18 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello Django")
+    if request.method == 'POST':
+        first_value = int(request.POST['num1'])
+        second_value = int(request.POST['num2'])
+        add = first_value + second_value
+        print(add)
+        context = {
+            'add':add
+        }
+        return render(request,'polls/ans.html',context)
+    else:
+        return render(request,'polls/index.html')
+    
+
+def detail(request):
+    return HttpResponse("detail page")
